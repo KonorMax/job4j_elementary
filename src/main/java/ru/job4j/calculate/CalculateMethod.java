247bfc01363a3c7fc93f2b3c9d1b7fdfe0d99b98
Максим Кондрашов <konor.max@mail.ru>
2022-10-23 22:11:44 +0300

    4. Методы [#231984]

---
+++ src/main/java/ru/job4j/calculate/CalculateMethod.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculate;
+
+public class CalculateMethod {
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        CalculateMethod.plus(100, 500);
+        CalculateMethod.plus(4, 2);
+        CalculateMethod.plus(3, 5);
+    }
+}
