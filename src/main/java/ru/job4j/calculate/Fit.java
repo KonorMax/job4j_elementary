16b2e6960eb94ee21839ce2ddb610bf497111cd8
Максим Кондрашов <konor.max@mail.ru>
2022-10-24 13:33:10 +0300

    6. Идеальный вес [#41584]

--- src/main/java/ru/job4j/calculate/Fit.java
+++ src/main/java/ru/job4j/calculate/Fit.java
@@ -16,9 +16,9 @@ public class Fit {
         short height = 187;
         double man = Fit.manWeight(height);
         System.out.println("Man 187 is " + man);
-        short height1 = 167;
+        short height1 = 177;
         double woman = Fit.womanWeight(height1);
-        System.out.println("Woman 167 is " + woman);
+        System.out.println("Woman 177 is " + woman);
 
     }
 }
