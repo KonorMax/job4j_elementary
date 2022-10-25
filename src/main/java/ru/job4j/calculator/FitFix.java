b32326e675ebb9638f1a49e9c254e743903fc2fb
Максим Кондрашов <konor.max@mail.ru>
2022-10-25 17:42:22 +0300

    Входные данные в тесте [#297750]

--- src/main/java/ru/job4j/calculator/FitFix.java
+++ src/main/java/ru/job4j/calculator/FitFix.java
@@ -1,4 +1,4 @@
-package ru.job4j.calculater;
+package ru.job4j.calculator;
 
 public class FitFix {
 
--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,4 +1,4 @@
-package ru.job4j.calculater;
+package ru.job4j.calculator;
 
 
 import static ru.job4j.math.MathFunction.*;
